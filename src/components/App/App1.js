0a84654ce9325d3a4ce6940a93c222416ac1a565
Vitalii-Dzoma <vitoos900@gmail.com>
2022-02-17 22:45:36 +0200

    пагинация

--- src/components/App/App1.js
+++ src/components/App/App1.js
@@ -5,47 +5,65 @@ import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 // import Loader from './Loader/Loader';
 // import Modal from './Modal/Modal';
 import Button from 'components/Button/Button';
-
+import { fetchImageCards } from '../../api/Api';
 class App1 extends React.Component {
-    state = {
-        image: null,
-        name: '',
-        count:1
-    }
-//   async componentDidMount() {
-//     const response = await fetch(
-//       `https://pixabay.com/api/?q=${this.props}&page=1&key=24642073-b12941d2e5507c49f57c90e2e&image_type=photo&orientation=horizontal&per_page=12`
-//     );
-//     const image = await response.json();
-//     console.log(image.hits);
-//     const arrayPokemon = await this.setState({ image });
-//     }
-    
-  formSubmitHandler = (data, page=1) => {
-      
-      fetch(`https://pixabay.com/api/?q=${data.name}&page=${page}&key=24642073-b12941d2e5507c49f57c90e2e&image_type=photo&orientation=horizontal&per_page=12`)
-          .then(res => res.json())
-          .then(image =>  this.setState({image, name:data.name})
-      )
+  state = {
+    image: null,
+    name: '',
+    count: 1,
+  };
+  //   async componentDidMount() {
+  //     const response = await fetch(
+  //       `https://pixabay.com/api/?q=${this.props}&page=1&key=24642073-b12941d2e5507c49f57c90e2e&image_type=photo&orientation=horizontal&per_page=12`
+  //     );
+  //     const image = await response.json();
+  //     console.log(image.hits);
+  //     const arrayPokemon = await this.setState({ image });
+  //     }
+
+  formSubmitHandler = data => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+    fetchImageCards(data.name, this.state.count).then(image =>
+      this.setState({ image: image, name: data.name })
+    );
+
+    return this.state.image;
+    // fetch(
+    //   `https://pixabay.com/api/?q=${data.name}&page=${page}&key=24642073-b12941d2e5507c49f57c90e2e&image_type=photo&orientation=horizontal&per_page=12`
+    // )
+    //   .then(res => res.json())
+    //   .then(image => this.setState({ image: image.hits, name: data.name }));
+    // return this.state.image;
+  };
+  // async getData() {
+  //   const dataArray = await fetchImageCards(this.state.name, this.state.count);
+  //   this.setState(prevState => ({
+  //     count: prevState.count + 1,
+  //     image: dataArray,
+  //   }));
+  // }
+
+  completeRender = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+    fetchImageCards(this.state.name, this.state.count).then(image =>
       this.setState(prevState => ({
-        count: prevState.count + 1
-        // image:[...prevState.image.hits, image.hits]
-      }));
+        image: [...prevState.image, ...image],
+      }))
+    );
+    // this.setState(({ image }) => ({
+    //   image: [...image, ...newData],
+    // }));
   };
 
-    completeRender = () => {
-        const newData = { ...image }
-         this.setState(({ image }) => ({
-           image: [...data, newData],
-         }));
-}
+  render() {
+    console.log(this.state.image);
+    if (this.state.image) {
+    }
 
-    
-    render() {
-        if (this.state.image) {
-            console.log(this.state.image.hits)
-        }
-    
     return (
       <>
         <Searchbar onSubmit={this.formSubmitHandler} />
@@ -53,10 +71,7 @@ class App1 extends React.Component {
         <ImageGallery>
           <ImageGalleryItem images={this.state.image} />
         </ImageGallery>
-        <Button
-          images={this.state.image}
-          onClick={() => this.formSubmitHandler(this.state, this.state.count)}
-        />
+        <Button images={this.state.image} onClick={this.completeRender} />
         {/* <Modal/> */}
       </>
     );
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
-
-const ImageGalleryItem = ({images}) => {
-    if (images) {
-     
-    }
-    return (
-      <>
-        {images &&
-          images.hits.map(image =>  (
-            <li className="gallery-item" key={image.id}>
-              <img src={image.webformatURL} alt={image.tags} />
-              </li>
-          )
-         )}
-      </>
-    );
-}
+const ImageGalleryItem = ({ images }) => {
+  if (images) {
+  }
+  return (
+    <>
+      {images &&
+        images.map(image => (
+          <li className="gallery-item" key={image.id}>
+            <img src={image.webformatURL} alt={image.tags} />
+          </li>
+        ))}
+    </>
+  );
+};
 
 export default ImageGalleryItem;
